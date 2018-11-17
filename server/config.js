0b85a6b6bb3eded00d8ee61ba070dddbd8fa87e3
David <davidgcastro93@gmail.com>
2018-11-17 18:30:13 -0500

    translates text and sends to client side

--- server/config.js
+++ server/config.js
@@ -1,6 +1,7 @@
 const config = {
   apiKey: 'AIzaSyCQPWtt4L-I4xd1VWpdqqYSwDT18FBK69A',
-  baseUrl: 'https://speech.googleapis.com/v1/speech:recognize'
+  baseUrl: 'https://speech.googleapis.com/v1/speech:recognize',
+  projectid: 'wdfhackathon'
 };
 
 module.exports = config;
