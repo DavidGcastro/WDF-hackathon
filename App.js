d97756d610a1f77311994e5db39ac1bd361b7d72
Jacob Feinberg <JacobFeinberg92@gmail.com>
2018-11-17 12:21:37 -0500

    refactor(app) Use anonymous functions in onValueChange

--- App.js
+++ App.js
@@ -19,12 +19,12 @@ export default class App extends React.Component {
     return (
       <View style={styles.container}>
         <Text>Open up App.js to start working on your app!</Text>
-        <Picker style={{ height: 50, width: 300 }} selectedValue={this.state.fromLanguage} onValueChange={this.handleChange}>
+        <Picker style={{ height: 50, width: 300 }} selectedValue={this.state.fromLanguage} onValueChange={(itemValue) =>  this.setState({fromLanguage: itemValue})}>
           <Picker.Item label="Choose Speaker's Langauge" value="" />
           <Picker.Item label="English" value="English" />
           <Picker.Item label="Spanish" value="Spanish" />
         </Picker>
-        <Picker style={{ height: 50, width: 300 }} selectedValue={this.state.fromLanguage} onValueChange={this.handleChange}>
+        <Picker style={{ height: 50, width: 300 }} selectedValue={this.state.translationLanguage} onValueChange={(itemValue) =>  this.setState({translationLanguage: itemValue})}>
           <Picker.Item label="Choose Translation Langauge" value="" />
           <Picker.Item label="Spanish" value="Spanish" />
           <Picker.Item label="English" value="English" />
