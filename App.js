41691aaeb475b75c4a3d15da4c2956c50c24e3b6
Jacob Feinberg <JacobFeinberg92@gmail.com>
2018-11-17 11:40:51 -0500

    feat(app) Create picker for languages

--- App.js
+++ App.js
@@ -1,11 +1,15 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Picker } from 'react-native';
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
         <Text>Open up App.js to start working on your app!</Text>
+        <Picker style={{ height: 50, width: 100 }}>
+          <Picker.Item label="English" value="English" />
+          <Picker.Item label="Spanish" value="Spanish" />
+        </Picker>
       </View>
     );
   }
