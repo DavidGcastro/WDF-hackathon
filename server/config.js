4c80c3a6b290b54a328a0f7b61281f6855f5a5d0
David <davidgcastro93@gmail.com>
2018-11-17 16:59:45 -0500

    update config, update server

--- server/config.js
+++ server/config.js
@@ -1,20 +1,6 @@
 const config = {
   apiKey: 'AIzaSyCQPWtt4L-I4xd1VWpdqqYSwDT18FBK69A',
-  baseUrl:
-    'https://speech.googleapis.com/v1/speech:recognize?key=AIzaSyCQPWtt4L-I4xd1VWpdqqYSwDT18FBK69A',
-
-  audio: {
-    uri: 'gs://speech-demo/shwazil_hoful.flac'
-  },
-  config: {
-    encoding: 'FLAC',
-    sampleRateHertz: 16000,
-    languageCode: 'en-US'
-  },
-  request: {
-    audio: this.audio,
-    config: this.config
-  }
+  baseUrl: 'https://speech.googleapis.com/v1/speech:recognize'
 };
 
 module.exports = config;
--- server/index.js
+++ server/index.js
@@ -2,32 +2,34 @@ const express = require('express');
 const app = express();
 const speech = require('@google-cloud/speech');
 const config = require('./config');
+const bodyParser = require('body-parser');
 const client = new speech.SpeechClient();
-const request = {
-  config: config.config,
-  audio: config.audio
-};
+app.use(bodyParser.json({ limit: '10mb', extended: true }));
+app.use(bodyParser.urlencoded({ limit: '10mb', extended: true }));
 
 app.post('/', (req, res, next) => {
-    console.log(req.body, 555555);
-    res.send('HIT');
+  const request = {
+    config: {
+      encoding: 'FLAC',
+      sampleRateHertz: 16000,
+      languageCode: req.body.language,
+      model: 'default'
+    },
+    audio: { content: req.body.recordingAsString }
+  };
+
+  client
+    .recognize(request)
+    .then(data => {
+      console.log(data);
+      res.send(data);
+    })
+    .catch(err => {
+      console.error('ERROR:', err);
+      next(err);
+    });
 });
 
 app.listen(3000, function() {
   console.log('Listening on 3000');
 });
-
-
-
-client
-  .recognize(request)
-  .then(data => {
-    const response = data[0];
-    const transcription = response.results
-      .map(result => result.alternatives[0].transcript)
-      .join('\n');
-    console.log(`Transcription: `, transcription);
-  })
-  .catch(err => {
-    console.error('ERROR:', err);
-  });
