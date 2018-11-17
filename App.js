ba5dfc215cde010cff13f4a5629d10c7c2d69178
Jacob Feinberg <JacobFeinberg92@gmail.com>
2018-11-17 13:38:09 -0500

    refactor(app) Use destructured properties from state

--- App.js
+++ App.js
@@ -17,14 +17,15 @@ export default class App extends React.Component {
   }
 
   render() {
+    const {fromLanguage, translationLanguage, isActive} = this.state
     return (
       <View style={styles.container}>
-        <Picker style={{ height: 50, width: 300, bottom: 200 }} selectedValue={this.state.fromLanguage} onValueChange={(itemValue) =>  this.setState({fromLanguage: itemValue})}>
+        <Picker style={{ height: 50, width: 300, bottom: 200 }} selectedValue={fromLanguage} onValueChange={(itemValue) =>  this.setState({fromLanguage: itemValue})}>
           <Picker.Item label="Choose Speaker's Langauge" value="" />
           <Picker.Item label="English" value="English" />
           <Picker.Item label="Spanish" value="Spanish" />
         </Picker>
-        <Picker style={{ height: 50, width: 300, bottom: 100 }} selectedValue={this.state.translationLanguage} onValueChange={(itemValue) =>  this.setState({translationLanguage: itemValue})}>
+        <Picker style={{ height: 50, width: 300, bottom: 100 }} selectedValue={translationLanguage} onValueChange={(itemValue) =>  this.setState({translationLanguage: itemValue})}>
           <Picker.Item label="Choose Translation Langauge" value="" />
           <Picker.Item label="Spanish" value="Spanish" />
           <Picker.Item label="English" value="English" />
