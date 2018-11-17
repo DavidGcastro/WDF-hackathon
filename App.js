3578ec51c32298ed4efca83974ba5e36f63ec813
Jacob Feinberg <JacobFeinberg92@gmail.com>
2018-11-17 11:48:40 -0500

    feat(app) Add constructor and local state with fromLanguage property

--- App.js
+++ App.js
@@ -2,6 +2,12 @@ import React from 'react';
 import { StyleSheet, Text, View, Picker } from 'react-native';
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      fromLanguage: ''
+    }
+  }
   render() {
     return (
       <View style={styles.container}>
