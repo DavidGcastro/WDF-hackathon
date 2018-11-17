a2e8b66f90fd0e15f28b21d0b56ca3b628bbfc96
Jacob Feinberg <JacobFeinberg92@gmail.com>
2018-11-17 11:52:35 -0500

    style(app) Increase width of picker

--- App.js
+++ App.js
@@ -12,8 +12,8 @@ export default class App extends React.Component {
     return (
       <View style={styles.container}>
         <Text>Open up App.js to start working on your app!</Text>
-        <Picker style={{ height: 50, width: 100 }}>
-          <Picker.Item label="Choose Speakers Langauge" value="" />
+        <Picker style={{ height: 50, width: 300 }}>
+          <Picker.Item label="Choose Speaker's Langauge" value="" />
           <Picker.Item label="English" value="English" />
           <Picker.Item label="Spanish" value="Spanish" />
         </Picker>
