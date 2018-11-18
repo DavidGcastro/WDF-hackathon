9ea2e58e0009c8bf57ef7b0e54baf71503e197a7
David <davidgcastro93@gmail.com>
2018-11-18 00:37:54 -0500

    Git ignored secrets dir

--- server/secrets/config.js
+++
@@ -1,7 +0,0 @@
-const config = {
-  apiKey: 'AIzaSyDgCB1mdg94Ib1CeKmGI7-KOfMi6wrL-cU',
-  baseUrl: 'https://speech.googleapis.com/v1/speech:recognize',
-  projectid: 'wdfhackathon'
-};
-
-module.exports = config;
